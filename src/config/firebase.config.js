b6c43a1274937633c7785568214df6185641cc4b
stormchaser33 <nicolasthiry860@gmail.com>
2023-08-28 14:13:55 +0200

    basic modal layout

--- src/config/firebase.config.js
+++ src/config/firebase.config.js
@@ -1,25 +1,24 @@
 const firebaseConfig = {
-  firebase: 
-    process.env.NODE_ENV === 'production' ? 
-      {
+  firebase:
+    process.env.NODE_ENV === "production"
+      ? {
         apiKey: "AIzaSyDsBOgwkPAp9voyXz7tt6ra-c8P6902Fug",
         authDomain: "suarte.firebaseapp.com",
         projectId: "suarte",
         storageBucket: "suarte.appspot.com",
         messagingSenderId: "767194707735",
         appId: "1:767194707735:web:78ac9e18d0f4f839c72952",
-        measurementId: "G-WND0DFNW2G"
-      } 
-      : 
-      {
-        apiKey: "AIzaSyDc_nhaIZ5grASKbNLsNmnRIO7fxpZEamo",
-        authDomain: "suarte-pruebas.firebaseapp.com",
-        projectId: "suarte-pruebas",
-        storageBucket: "suarte-pruebas.appspot.com",
-        messagingSenderId: "695116734938",
-        appId: "1:695116734938:web:6aeb3eff8877b9a296a0e3",
-        measurementId: "G-HN8D6QXX3P"
-      }      
+        measurementId: "G-WND0DFNW2G",
+      }
+      : {
+        apiKey: "AIzaSyAos3aIItpB80DqQgyWUP-oKd2Bph2HhsI",
+        authDomain: "suarte-pruebas-6cc24.firebaseapp.com",
+        projectId: "suarte-pruebas-6cc24",
+        storageBucket: "suarte-pruebas-6cc24.appspot.com",
+        messagingSenderId: "518594067204",
+        appId: "1:518594067204:web:1e928aacdd68497c6eee51",
+        measurementId: "G-NKF8W1KNL9",
+      },
 };
 
 export default firebaseConfig;
