7cd23545dee2379c8011d92bb7f09e7ccf6fc3d0
stormchaser33 <nicolasthiry860@gmail.com>
2023-09-06 12:37:29 +0200

    feat: task 4 basic complete

--- src/layouts/navigation/user-profile/components/ProfileNavigation.jsx
+++ src/layouts/navigation/user-profile/components/ProfileNavigation.jsx
@@ -1,5 +1,10 @@
+import { useContext } from 'react';
 import { NavLink, useLocation } from 'react-router-dom';
 
+import config from '@/config';
+import ProfileDataContext from '@/context/ProfileDataContext';
+import useCache from '@/hooks/useCache';
+import useQuery from '@/hooks/useQuery';
 import RippleButton from '@/shared-components/buttons/components/RippleButton';
 import ArtistIcon from '@/shared-components/icons/components/user-profile/ArtistIcon';
 import ArtlistIcon from '@/shared-components/icons/components/user-profile/ArtlistIcon';
@@ -39,82 +44,138 @@ function ProfileNavigationTab({ to, tabName, tabIcon }) {
 }
 
 export default function ProfileNavigation({ fetchData }) {
+  const { profileData, internal } = useContext(ProfileDataContext);
+  const { fetchData: artistsFetchData } = useCache(`${profileData._id}_artists`, `${config.apis.api.url}/gallery/${profileData._id}/artists`, {
+    injectToken: true,
+    invalidateWhen: internal ? 
+      ["BUY_ARTWORK", "EDIT_ARTWORK", "DELETE_ARTWORK", "NEW_ARTIST", `${profileData._id}_REFRESH`] 
+      : 
+      [`${profileData._id}_REFRESH`]
+  });
+
+  const { queryData: artworksQueryData } = useQuery(`${profileData._id}_artworks`, `${config.apis.api.url}/artworks/${profileData._id}`, "", {
+    invalidateWhen: internal ? 
+      ["BUY_ARTWORK", "EDIT_ARTWORK", "DELETE_ARTWORK", `${profileData._id}_REFRESH`] 
+      : 
+      [`${profileData._id}_REFRESH`]  
+  });
+
+  const { fetchData: artlistsFetchData } = useCache(`${profileData._id}_artlists`, `${config.apis.api.url}/artlist/user/${profileData._id}`, {
+    injectToken: true,
+    invalidateWhen: internal ? 
+      ["NEW_ARTLIST", "EDIT_ARTLIST", "DELETE_ARTLIST", "ADD_TO_ARTLIST", "ADD_TO_ARTLIST_FROM_ARTWORK", "REMOVE_FROM_ARTLIST", `${profileData._id}_REFRESH`] 
+      : 
+      [`${profileData._id}_REFRESH`]
+  });
+
+  const { fetchData: boardFetchData } = useCache(`${profileData._id}_posts`, `${config.apis.api.url}/posts/gallery/${profileData._id}`, {
+    injectToken: true,
+    invalidateWhen: internal ? 
+      ["SUBMIT_POST", "DELETE_POST", `${profileData._id}_REFRESH`] 
+      : 
+      [`${profileData._id}_REFRESH`]
+  });
+
+  const { fetchData: collectionFetchData } = useCache(`${profileData._id}_collection`, `${config.apis.api.url}/collection/${profileData._id}`, {
+    injectToken: true,
+    includeSearchQueries: { visibility: "private" }, 
+    invalidateWhen: internal ? 
+      ["BUY_ARTWORK", "EDIT_COLLECTION", `${profileData._id}_REFRESH`] 
+      : 
+      [`${profileData._id}_REFRESH`]
+  });
+
+  const { fetchData: thoughtsFetchData } = useCache(`${profileData._id}_thoughts`, `${config.apis.api.url}/thought/user/${profileData._id}`, {
+    injectToken: true,
+    invalidateWhen: internal ? 
+      ["NEW_THOUGHT", "DELETE_THOUGHT", `${profileData._id}_REFRESH`] 
+      : 
+      [`${profileData._id}_REFRESH`]
+  });
+
+  const { fetchData: ratingFetchData } = useCache(`${profileData._id}_ratings`, `${config.apis.api.url}/rating/user/${profileData._id}`, {
+    injectToken: true,
+    invalidateWhen: ["NEW_RATING", `${profileData._id}_REFRESH`]
+  });
+
+  console.log("artists:", artistsFetchData, "artworks:", artworksQueryData, "artlists:", artlistsFetchData, "board:", boardFetchData, "collection:", collectionFetchData, "thoughts:", thoughtsFetchData, "rating:", ratingFetchData);
+
   if(fetchData.user_type === "gallery") {
     return (
       <div className="profile-navigation__container">
-        <ProfileNavigationTab
+        {(internal || (artworksQueryData && artworksQueryData?.data.data.length > 0)) && <ProfileNavigationTab
           to="artworks"
           tabName="Artworks"
           tabIcon={<ArtworkIcon className="profile-navigation__tab-icon artworks"/>}
-        />
+        />}
 
-        <ProfileNavigationTab
+        {(internal || (artistsFetchData && artistsFetchData.data.length > 0)) && <ProfileNavigationTab
           to="artists"
           tabName="Artists"
           tabIcon={<ArtistIcon className="profile-navigation__tab-icon artists"/>}
-        />
+        />}
 
-        <ProfileNavigationTab
+        {(internal || (artlistsFetchData && artlistsFetchData.data.length > 0)) && <ProfileNavigationTab
           to="artlists"
           tabName="Artlists"
           tabIcon={<ArtlistIcon className="profile-navigation__tab-icon artlists"/>}
-        />
+        />}
 
-        <ProfileNavigationTab
+        {(internal || (boardFetchData && boardFetchData.data.length > 0)) && <ProfileNavigationTab
           to="board"
           tabName="Board"
           tabIcon={<BoardIcon className="profile-navigation__tab-icon board"/>}
-        />
+        />}
       </div>
     );
   } else if(fetchData.user_type === "artist") {
     return (
       <div className="profile-navigation__container">
-        <ProfileNavigationTab
+        {(internal || (artworksQueryData && artworksQueryData.data.data.length > 0)) && <ProfileNavigationTab
           to="artworks"
           tabName="Artworks"
           tabIcon={<ArtworkIcon className="profile-navigation__tab-icon artworks"/>}
-        />
+        />}
 
-        <ProfileNavigationTab
+        {(internal || (collectionFetchData && collectionFetchData.data.length > 0)) && <ProfileNavigationTab
           to="collection"
           tabName="Collection"
           tabIcon={<CollectionIcon className="profile-navigation__tab-icon collection"/>}
-        />
+        />}
 
-        <ProfileNavigationTab
+        {(internal || (thoughtsFetchData && ratingFetchData) && (thoughtsFetchData.data.length > 0 || ratingFetchData.data.length > 0)) && <ProfileNavigationTab
           to="community"
           tabName="Community"
           tabIcon={<CommunityIcon className="profile-navigation__tab-icon community"/>}
-        />
+        />}
 
-        <ProfileNavigationTab
+        {(internal || (artlistsFetchData && artlistsFetchData.data.length > 0)) && <ProfileNavigationTab
           to="artlists"
           tabName="Artlists"
           tabIcon={<ArtlistIcon className="profile-navigation__tab-icon artlists"/>}
-        />
+        />}
       </div>
     );
   } else {
     return (
       <div className="profile-navigation__container">
-        <ProfileNavigationTab
+        {(internal || (collectionFetchData && collectionFetchData.data.length > 0)) && <ProfileNavigationTab
           to="collection"
           tabName="Collection"
           tabIcon={<CollectionIcon className="profile-navigation__tab-icon collection"/>}
-        />
+        />}
 
-        <ProfileNavigationTab
+        {(internal || (thoughtsFetchData && ratingFetchData)&&(thoughtsFetchData.data.length > 0 || ratingFetchData.data.length> 0)) && <ProfileNavigationTab
           to="community"
           tabName="Community"
           tabIcon={<CommunityIcon className="profile-navigation__tab-icon community"/>}
-        />
+        />}
 
-        <ProfileNavigationTab
+        {(internal || (artlistsFetchData && artlistsFetchData.data.length > 0)) && <ProfileNavigationTab
           to="artlists"
           tabName="Artlists"
           tabIcon={<ArtlistIcon className="profile-navigation__tab-icon artlists"/>}
-        />
+        />}
       </div>
     );
   }
--- src/pages/profile/community/index.jsx
+++ src/pages/profile/community/index.jsx
@@ -3,6 +3,7 @@ import { useLocation } from 'react-router-dom';
 
 import ProfileDataContext from '@/context/ProfileDataContext';
 import useGetSearchParams from '@/hooks/useGetSearchParams';
+import useStateHandler from '@/hooks/useStateHandler';
 import Text from '@/shared-components/text/components/Text';
 
 import Ratings from './ratings';
@@ -12,9 +13,12 @@ import './index.css';
 
 export default function ProfileCommunity() {
   const { profileData, internal } = useContext(ProfileDataContext);
+  const { cacheHandler } = useStateHandler();
   const [params, setParams] = useGetSearchParams({ validParams: ["v"] });
   const [view, setView] = useState(params ? params : { v: "thoughts" });
   const location = useLocation();
+  const thoughtsCacheValue = cacheHandler.getCacheValue(`${profileData._id}_thoughts`)?.data;
+  const ratingsCacheValue = cacheHandler.getCacheValue(`${profileData._id}_ratings`)?.data;
 
   function changeView(view) {
     const newView = { v: view };
@@ -25,21 +29,29 @@ export default function ProfileCommunity() {
   return (
     <>
       <div className="user-profile__community-navigation element-non-selectable">
-        <div className={`user-profile__community-navigation-link ${view.v === "thoughts" ? "active" : ""}`} onClick={()=>changeView("thoughts")}>
-          <Text className="user-profile__community-navigation-link-text" extraSmall>
-                        Thoughts
-          </Text>
-        </div>
+        {
+          (internal || (thoughtsCacheValue && thoughtsCacheValue.data.length > 0)) && (
+            <div className={`user-profile__community-navigation-link ${view.v === "thoughts" ? "active" : ""}`} onClick={()=>changeView("thoughts")}>
+              <Text className="user-profile__community-navigation-link-text" extraSmall>
+                            Thoughts
+              </Text>
+            </div>
+          )
+        }
 
-        <div className={`user-profile__community-navigation-link ${view.v === "ratings" ? "active" : ""}`} onClick={()=>changeView("ratings")}>
-          <Text className="user-profile__community-navigation-link-text" extraSmall>
-                        Ratings
-          </Text>
-        </div>
+        {
+          (internal || (ratingsCacheValue && ratingsCacheValue.data.length > 0)) && (
+            <div className={`user-profile__community-navigation-link ${view.v === "ratings" ? "active" : ""}`} onClick={()=>changeView("ratings")}>
+              <Text className="user-profile__community-navigation-link-text" extraSmall>
+                            Ratings
+              </Text>
+            </div>
+          )
+        }
       </div>
 
-      {view.v === "thoughts" && <Thoughts profileData={profileData} internal={internal}/>}
-      {view.v === "ratings" && <Ratings profileData={profileData} internal={internal}/>}
+      {thoughtsCacheValue && thoughtsCacheValue.data.length > 0 && view.v === "thoughts" && <Thoughts profileData={profileData} internal={internal}/>}
+      {ratingsCacheValue && ratingsCacheValue.data.length > 0 && view.v === "ratings" && <Ratings profileData={profileData} internal={internal}/>}
     </>
   );
 }
