0209d4e1b02c220082684b207c8b651a6e793fcd
nicolas-thiry <nicolas.thiry0418@gmail.com>
2023-10-12 16:05:48 +0200

    Updated Notification form styles

--- src/pages/notifications/components/alerttab/alerttab.jsx
+++ src/pages/notifications/components/alerttab/alerttab.jsx
@@ -4,14 +4,14 @@ export default function AlertTab( { children, className="" } ) {
   return (
     <>
       <div className={"notification__tab__container " + className}>
-        <div className='notification__tab'>
+        <div className='notification__tab active'>
           <p className='notification__tab__title'>All</p>
         </div>
         <div className='notification__tab'>
-          <p className='notification__tab__title'>All</p>
+          <p className='notification__tab__title'>Interactions</p>
         </div>
         <div className='notification__tab'>
-          <p className='notification__tab__title'>All</p>
+          <p className='notification__tab__title'>From Suarte</p>
         </div>
         { children }
       </div>
