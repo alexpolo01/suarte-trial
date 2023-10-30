a983111642b528ea9f78cd092fd3771a8fd2c19b
stormchaser35 <ciprian.boicea1016@outlook.com>
2023-10-30 10:49:55 -0400

    feat: Admin Panel UI

---
+++ src/pages/admin/home-customization/index.jsx
@@ -0,0 +1,15 @@
+import './index.css';
+
+export default function HomeCustomization() {
+  return (
+    <div className="home-customization__container">
+      <div className="home-customization__box">
+        <a className="home-customization__link">Fresh Picks</a>
+        <a className="home-customization__link">Human Expression</a>
+        <a className="home-customization__link">Harmony</a>
+        <a className="home-customization__link">Color expression</a>
+        <a className="home-customization__link">ADD CAROUSEL</a>
+      </div>
+    </div>
+  );
+}
--- src/pages/admin/welcome-admin/index.jsx
+++ src/pages/admin/welcome-admin/index.jsx
@@ -6,11 +6,25 @@ export default function WelcomeAdmin() {
   return (
     <>
       <div className="welcome-admin__container">
-        <div className="welcome-admin__links">
-          <Link className="welcome-admin__link" to="gallery-requests">Ver solicitudes de galerias</Link>
-          <Link className="welcome-admin__link" to="artwork-requests" style={{ marginRight: "20px" }}>Ver solicitudes de cuadros</Link>
-          <Link className="welcome-admin__link" to="limited-edition-requests" style={{ marginRight: "20px" }}>Ver solicitudes de ediciones limitadas</Link>
-          <a className="welcome-admin__link" href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" style={{ marginRight: "20px" }}>Ver datos bancarios de Pablo Gil Martinez</a>
+        <div className="welcome-admin__box">
+          <div>
+            <h5 className="welcome-admin__box-header">REQUESTS</h5>
+          </div>
+          <div className="welcome-admin__links">
+            <Link className="welcome-admin__link" to="gallery-requests">Galleries</Link>
+            <Link className="welcome-admin__link" to="artwork-requests" style={{ marginRight: "20px" }}>Artworks</Link>
+            <Link className="welcome-admin__link" to="limited-edition-requests">Limited Editions</Link>
+            {/* <a className="welcome-admin__link" href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" style={{ marginRight: "20px" }}>Ver datos bancarios de Pablo Gil Martinez</a> */}
+          </div>
+        </div>
+        <div className="welcome-admin__box">
+          <div>
+            <h5 className="welcome-admin__box-header">CUSTOMIZATION</h5>
+          </div>
+          <div className="welcome-admin__links">
+            <Link className="welcome-admin__customization" to="customize-home">Home</Link>
+            <Link className="welcome-admin__customization" to="artwork-requests" style={{ marginLeft: "20px" }}>Search</Link>
+          </div>
         </div>
       </div>
     </>
