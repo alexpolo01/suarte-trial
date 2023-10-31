4c6b4d41002a9aa35e88437a6e1d08e3478d535e
stormchaser35 <ciprian.boicea1016@outlook.com>
2023-10-31 11:19:44 -0400

    ui: admin home customization

--- src/pages/admin/home-customization/index.jsx
+++ src/pages/admin/home-customization/index.jsx
@@ -2,9 +2,20 @@
 import { useNavigate } from 'react-router-dom';
 import './index.css';
 import BackArrowIcon from '@/shared-components/icons/components/public/BackArrowIcon';
+import { useEffect, useState } from 'react';
+import PublicFormInput from '@/shared-components/inputs/components/PublicFormInput';
+import ContinueButton from '@/shared-components/buttons/components/ContinueButton';
+import AddArtworksPopup from '../add-artworks-popup';
 
 export default function HomeCustomization() {
   const navigate = useNavigate();
+  const [step, setStep] = useState(0);
+  const [error, setError] = useState(null);
+  const [popupOpen, setPopupOpen] = useState(false);
+
+  useEffect(() => {
+    setError(null); 
+  }, []);
 
   const onClickBack = () => {
     navigate(-1);
@@ -13,17 +24,34 @@ export default function HomeCustomization() {
   return (
     <div className="home-customization__container">
       <div className="home-customization__header">
-        <BackArrowIcon className="home-customization__header-back" onClick={onClickBack}/>
+        <div className="home-customization__header-back">
+          <BackArrowIcon onClick={onClickBack}/>
+        </div>
         <h4 className="home-customization__header-text">Home</h4>
-        <div></div>
+        <div className="home-customization__header-logout">
+          <ContinueButton>
+            Log out
+          </ContinueButton>
+        </div>
       </div>
-      <div className="home-customization__box">
-        <a className="home-customization__link">Fresh Picks</a>
-        <a className="home-customization__link">Human Expression</a>
-        <a className="home-customization__link">Harmony</a>
-        <a className="home-customization__link">Color expression</a>
-        <a className="home-customization__link">ADD CAROUSEL</a>
-      </div>
+      { step === 0 && 
+        <div className="home-customization__box">
+          <a className="home-customization__link">Fresh Picks</a>
+          <a className="home-customization__link">Human Expression</a>
+          <a className="home-customization__link">Harmony</a>
+          <a className="home-customization__link">Color expression</a>
+          <a className="home-customization__link" onClick={() => setStep(1)}>ADD CAROUSEL</a>
+        </div>
+      }
+      { step === 1 && 
+        <div>
+          <PublicFormInput className="home-customization__title-input" error={error} placeholder="Title" element="title" margin />
+          <ContinueButton onClick={() => setPopupOpen(true)}>
+            ADD ARTWORKS
+          </ContinueButton>
+        </div>
+      }
+      <AddArtworksPopup open={popupOpen} onClose={() => setPopupOpen(false)} />
     </div>
   );
 }
