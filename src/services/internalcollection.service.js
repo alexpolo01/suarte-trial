da2c6b17db660abd2f949593880b7840c0b04cf8
stormchaser35 <ciprian.boicea1016@outlook.com>
2023-11-09 01:39:13 -0500

    feat: update admin home

--- src/services/internalcollection.service.js
+++ src/services/internalcollection.service.js
@@ -2,7 +2,13 @@ import config from '@/config';
 import fetchWrapper from '@/services/fetchWrapper.service';
 
 function updateArtworksOrder(data, id) {
-  return fetchWrapper.put(`${config.apis.api.url}/internalcollection/updateorder/${id}`, {
+  return fetchWrapper.put(`${config.apis.api.url}/internalcollection/updateartworksorder/${id}`, {
+    injectToken: true,
+    body: data
+  });
+}
+function updateCollectionsOrder(data, is_mobile) {
+  return fetchWrapper.put(`${config.apis.api.url}/internalcollection/updateorder/${is_mobile}`, {
     injectToken: true,
     body: data
   });
@@ -10,6 +16,7 @@ function updateArtworksOrder(data, id) {
 
 const InternalCollectionService = {
   updateArtworksOrder,
+  updateCollectionsOrder
 };
 
 export default InternalCollectionService;
